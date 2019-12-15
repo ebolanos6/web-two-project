40a8be8b530db81bd33dcfef5be08cc94c023efa
Eric Bolanos <ebolanos6@student.alamo.edu>
2019-12-15 03:09:19 -0600

    final

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $( document ).ready(function() {
-  
 
-    
+    $('button').click(function () {
+        $('h1').addClass('hide');
+        });
 
-
-
-
-
+    $('button').click(function () {
+        $('h1').removeClass('show');
+        })
+            
+    setTimeout(function () {
+         $('h1').addClass('show');
+        }, 2000);
+                
+               
 })
